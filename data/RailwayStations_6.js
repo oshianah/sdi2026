9a0a144cf62e13a284f51faa2d42b7244bef28d4
oshianah <oshiarakg1@gmail.com>
2026-05-20 16:11:56 +0200

    Add files via upload

---
+++ data/RailwayStations_6.js
@@ -0,0 +1,8 @@
+var json_RailwayStations_6 = {
+"type": "FeatureCollection",
+"name": "RailwayStations_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1.0, "objectid_1": 0.0, "name": "Mowbray" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 18.473910075810949, -33.946658364982426 ] ] } }
+]
+}
